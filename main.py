626c14a06c2e8248497039aa2067e70abdbf5ce6
shrigopalsarkar <shrigopalsarkar@gmail.com>
2026-08-12 21:47:18 +0530

    Update main.py

--- main.py
+++ main.py
@@ -68,7 +68,7 @@ def generate_report(iso_datetime: str, lang: str = "bn"):
         """
 
         response = client.models.generate_content(
-            model='gemini-2.0-flash',
+            model='gemini-2.5-flash',
             contents=prompt,
         )
 
