f517489070d8b65ac3120816901164d8d0fe9ad5
shrigopalsarkar <shrigopalsarkar@gmail.com>
2026-08-24 16:08:31 +0530

    Fix syntax error in main.py

--- main.py
+++ main.py
@@ -680,6 +680,7 @@ async def get_panchang(
         },
         # panchang.py এর ১০০% সঠিক ফলাফল অনুবাদসহ রিটার্ন
         "mantri_mandal": get_localized_mantri_mandala(date_obj, lat, lon, lang=lang)
+        "choghadiya": compute_choghadiya(date_obj, rise_min, set_min, lang=lang)
     }
 
 @app.get("/calculate")
